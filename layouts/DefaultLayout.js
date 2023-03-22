f8a09fb5dc8e88a44b71ed0f6da80820449f93d8
Ryan White <white.ryanw@gmail.com>
2023-03-21 22:14:34 -0400

    Got Accustomed to changes, going to merge main

--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "@/components/Header/Header.js";
-import Image from "next/image";
+
 function DefaultLayout({ children }) {
   return (
     <div className="default-layout">
