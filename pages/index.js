bc886ea006ee6e609e910e068deb9080166eb22a
Ryan White <white.ryanw@gmail.com>
2023-11-11 21:48:12 -0800

    New link to meta & og logo in Head component

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Investant | Financial Tools, Literacy, & Education" />
         <meta name="twitter:description" content="Investant is a provider of independent financial research and tools for individuals who want to improve their financial literacy and education in today's economy." />
-        <meta name="twitter:image" content="images/branding/TransparentLogo.svg" />
+        <meta name="twitter:image" content="https://investant.net/images/branding/TransparentLogo.svg" />
 
         {/* Open Graph Meta Tags (for platforms like Facebook) */}
         <meta property="og:title" content="Investant | Financial Tools, Literacy, & Education" />
         <meta property="og:description" content="Investant is a provider of independent financial research and tools for individuals who want to improve their financial literacy and education in today's economy." />
-        <meta property="og:image" content="images/branding/TransparentLogo.svg" />
+        <meta property="og:image" content="https://investant.net/images/branding/TransparentLogo.svg" />
         <meta property="og:url" content="https://investant.net" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Investant" />
