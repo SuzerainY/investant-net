99a6471800329d35649a24a4d14cc8049fde04ec
Ryan White <white.ryanw@gmail.com>
2023-11-16 20:21:46 -0800

    Remove unecessary file

--- layouts/SeparateHeaderLayout.js
+++
@@ -1,9 +0,0 @@
-import Header from "@/components/Header/Header.js";
-
-export default function SeparateHeaderLayout() {
-  return (
-    <div className="default-header">
-      <Header/>
-    </div>
-  );
-}
