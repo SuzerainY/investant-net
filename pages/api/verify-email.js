867d41265da26c03535c30b47767dc4023f432dd
Ryan White <white.ryanw@gmail.com>
2024-05-30 23:43:12 -0700

    Verify Email API

---
+++ pages/api/verify-email.js
@@ -0,0 +1,24 @@
+import { STRAPIurl } from '@/my_modules/bloghelp';
+
+export default async function handler(req, res) {
+    const { confirmationToken } = req.query;
+
+    try {
+        // Verify the token and confirm the user
+        const response = await fetch(`${STRAPIurl}/api/auth/email-confirmation`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ confirmationToken }),
+        });
+
+        if (!response.ok) { throw new Error('Email Verification Failed.'); }
+
+        // Redirect to login page
+        res.writeHead(302, { Location: '/login' });
+        res.end();
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
